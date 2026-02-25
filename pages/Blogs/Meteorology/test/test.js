951123ca33e1990254992baa0f19ef45f1ec7f14
Connor Leatherdale <connorleatherdale@gmail.com>
2026-02-25 10:47:02 -0500

    still messing around

--- pages/Blogs/Meteorology/test/test.js
+++ pages/Blogs/Meteorology/test/test.js
@@ -11,3 +11,26 @@ function print(){
     document.getElementById("demo").innerHTML = math();
     console.log(a-1);
 }
+
+// Form functions and whatnot
+
+let x = 0;
+let y = 0;
+let z = 0;
+
+function input(){
+    // valueAsNumber is needed, if you try to use .value, when trying to add, treats them as strings, instead of integers, so the math then gets funky
+    x = document.getElementById('value1').valueAsNumber 
+    y = document.getElementById('value2').valueAsNumber
+}
+
+//changes the <p> to the output of z
+function printForm() {
+    let z = x + y;
+    document.getElementById("demo").innerHTML = z;
+}
+
+function form(x) {
+    document.getElementById("demo").innerHTML = x;
+}
+
