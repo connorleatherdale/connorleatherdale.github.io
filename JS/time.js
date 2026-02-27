e5b51dfb08f4e4298a7fb61b21b7221284707954
Connor Leatherdale <connorleatherdale@gmail.com>
2026-02-26 20:34:04 -0500

    moment

--- JS/time.js
+++ JS/time.js
@@ -2,17 +2,53 @@
 
 function updateTime(){
     var currentTime = new Date()
+    var year = currentTime.getFullYear()
+    var month = currentTime.getMonth()
+    var day = currentTime.getDay()
     var hours = currentTime.getHours()
     var minutes = currentTime.getMinutes()
+    
     if (minutes < 10){
         minutes = "0" + minutes
     }
     var t_str = hours + ":" + minutes + " ";
-    if(hours > 11){
+    if(hours > 11){ //is it morning or afernoon
         t_str += "PM";
     } else {
         t_str += "AM";
     }
+
+    // setup which month it is
+    t_str += ", ";
+    if (month == 0){
+        t_str += "January"
+    } else if (month == 1){
+        t_str += "February";
+    } else if (month == 2){
+        t_str += "March";
+    } else if (month == 3){
+        t_str += "April";
+    } else if (month == 4){
+        t_str += "May";
+    } else if (month == 5){
+        t_str += "June";
+    } else if (month == 6){
+        t_str += "July";
+    } else if (month == 7){
+        t_str += "Augest";
+    } else if (month == 8){
+        t_str += "September";
+    } else if (month == 9){
+        t_str += "October";
+    } else if (month == 10){
+        t_str += "November";
+    } else if (month == 11){
+        t_str += "December";
+    }
+
+    // day and year
+    t_str += ", " + day + ", " + year;
+
     document.getElementById('time_span').innerHTML = t_str;
 }
 setInterval(updateTime, 1000);
