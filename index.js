11af2c0c7a85c5b01c8f5fbf3e447b381ee8053d
Connor Leatherdale <connorleatherdale@gmail.com>
2026-04-20 14:59:36 -0400

    tried to add electron, didnt quite work out

---
+++ index.js
@@ -0,0 +1,27 @@
+// main.js
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+// IPC handlers
+ipcMain.handle('get-version', () => app.getVersion());
+ipcMain.handle('show-dialog', () => dialog.showMessageBox({ message: 'Hello!' }));
